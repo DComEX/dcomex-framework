5358cf5188d6210e9c526283dc6def15e88395ee
Sergey Litvinov <slitvinov@gmail.com>
2024-06-05 09:18:30 +0100

    update of MSolve

--- setup.py
+++
@@ -1,6 +0,0 @@
-import distutils.core
-
-distutils.core.setup(name='dcomex',
-                     version='0.1',
-                     py_modules=['follow', 'graph', 'kahan', 'remote'],
-                     url='https://github.com/DComEX/dcomex-framework')
